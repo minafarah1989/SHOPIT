f0ded5dec370392a60db8699be44a34dee10a675
mina farah <minafarah1989@gmail.com>
2021-04-06 13:57:36 +0200

    init

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const app = require('./app');
+const connectDatabase = require('./config/database');
+
+const dotenv = require('dotenv');
+
+// Handle Uncaught exceptions
+process.on('uncaughtException', err => {
+    console.error(`ERROR: ${err.stack}`);
+    console.log('shutting down server due to uncaught exception');
+    process.exit(1)
+})
+
+//setting up config file
+dotenv.config({path:'backend/config/config.env'});
+
+
+
+//connecting to database
+connectDatabase();
+
+const server= app.listen(process.env.PORT, ()=>{
+     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+ })
+
+ // Handle unhandled promise rejections
+ process.on('unhandledRejection',err => {
+     console.log(`ERROR: ${err.message}`);
+     console.log('Shutting down the server due to unhandled Promise rejection');
+     server.close(()=>{
+         process.exit(1)
+     });
+ })
